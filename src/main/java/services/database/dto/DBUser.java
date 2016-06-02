6141fc1024daad42793621a5163812abf229bc95
perran <hjelm.per@gmail.com>
2016-06-02 14:50:32 +0200

    added some hibernate stuff that dont work fully yet

---
+++ src/main/java/services/database/dto/DBUser.java
@@ -0,0 +1,37 @@
+package services.database.dto;
+
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+import org.hibernate.annotations.GenericGenerator;
+
+public class DBUser {
+	
+	@Id
+    @GeneratedValue(generator = "uuid")
+    @GenericGenerator(name = "uuid", strategy = "uuid2")
+	private String id;
+	
+	private String firstName;
+    private String lastName;
+        
+	public DBUser(String firstName, String lastName) {
+		super();
+		this.firstName = firstName;
+		this.lastName = lastName;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getFirstName() {
+		return firstName;
+	}
+
+	public String getLastName() {
+		return lastName;
+	}
+	
+	
+}
---
+++ src/test/java/services/UserDBTest.java
@@ -0,0 +1,52 @@
+package services;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import services.database.dto.DBUser;
+
+public class UserDBTest {
+
+	private static EntityManagerFactory entityManagerFactory;
+	
+	@BeforeClass
+    public static void setUpEntityManagerFactory() {
+        entityManagerFactory = Persistence.createEntityManagerFactory( "UserPersistanceUnit" );
+    }
+
+    @AfterClass
+    public static void closeEntityManagerFactory() {
+        entityManagerFactory.close();
+    }
+	
+	@Test
+	public void testUserDB(){
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		entityManager.getTransaction().begin();
+			
+		DBUser dbUser = new DBUser("Data", "Pata");
+		
+		entityManager.persist(dbUser);
+		
+		entityManager.close();
+		
+		//now read from db
+		
+		entityManager = entityManagerFactory.createEntityManager();
+		entityManager.getTransaction().begin();
+		
+		DBUser find = entityManager.find( DBUser.class, dbUser.getId() );
+		
+		Assert.assertThat(find.getFirstName(), org.hamcrest.CoreMatchers.is("Data"));
+		
+		entityManager.close();
+	}
+	
+}
